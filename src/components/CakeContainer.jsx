a256e5997280ffffca6b62b8a366e8c29062a68a
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2024-03-06 01:58:04 +0330

    css edited and cakecontainer added

---
+++ src/components/CakeContainer.jsx
@@ -0,0 +1,11 @@
+function CakeContainer() {
+  return (
+    <div>
+        <h2>Cake number : </h2>
+        <input type="number" />
+        <button>Buy cake</button>
+    </div>
+  )
+}
+
+export default CakeContainer
