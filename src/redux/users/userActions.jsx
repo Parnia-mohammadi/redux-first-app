86a03ed2ee4035adf449d7b4832ea01854652076
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2024-03-10 02:30:21 +0330

    userActions recoded in different way

--- src/redux/users/userActions.jsx
+++ src/redux/users/userActions.jsx
@@ -9,11 +9,26 @@ import {
 // }
 // export const fetchUsers = () => {return ()=>{}}
 export const fetchUsers = () => (dispatch) => {
-  dispatch({ type: Fetch_Users_Request });
+  // dispatch({ type: Fetch_Users_Request });
+  dispatch(fetchUsersRequest());
   axios
     .get("https://jsonplaceholder.typicode.com/users")
-    .then((res) => dispatch({ type: Fetch_Users_Success, payload: res.data }))
+    .then((res) =>
+      // dispatch({ type: Fetch_Users_Success, payload: res.data })
+      dispatch(fetchUsersSuccess(res.data))
+    )
     .catch((err) =>
-      dispatch({ type: Fetch_Users_Failure, payload: err.message })
+      // dispatch({ type: Fetch_Users_Failure, payload: err.message })
+      dispatch(fetchUsersFailure(err.message))
     );
 };
+
+function fetchUsersRequest() {
+  return { type: Fetch_Users_Request };
+}
+function fetchUsersSuccess(users) {
+  return { type: Fetch_Users_Success, payload: users };
+}
+function fetchUsersFailure(error) {
+  return { type: Fetch_Users_Failure, payload: error };
+}
