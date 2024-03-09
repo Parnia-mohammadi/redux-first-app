8eb748549c143339fa29acbffe4715a06876fcd8
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2024-03-10 01:38:38 +0330

    store enhancer from redux-thunk added & userReducer added in combineReducer

--- src/redux/rootReducer.jsx
+++ src/redux/rootReducer.jsx
@@ -1,6 +1,11 @@
 import { combineReducers } from "redux";
 import milkReducer from "./milk/milkReducer";
 import CakeReducer from "./cake/CakeReducer";
+import userReducer from "./users/userReducer";
 
-const rootReducers = combineReducers({cake:CakeReducer, milk:milkReducer});
+const rootReducers = combineReducers({
+  cake: CakeReducer,
+  milk: milkReducer,
+  users: userReducer,
+});
 export default rootReducers;
