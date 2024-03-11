ff25137fe26967f078cec7de61c630d074d467ba
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2024-03-11 16:31:26 +0330

    @redux-devtools/extensions added

--- src/redux/Store.jsx
+++ src/redux/Store.jsx
@@ -1,6 +1,7 @@
 import { applyMiddleware, createStore } from "redux";
 import rootReducers from "./rootReducer";
 import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
-const store = createStore(rootReducers, applyMiddleware(thunk));
+const store = createStore(rootReducers, composeWithDevTools(applyMiddleware(thunk)));
 export default store;
