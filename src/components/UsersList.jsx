828e2dda95a5749b9ec0ef08b4ce66b0e48f8042
parnia mohammadi <parniiiiia.mohammadi@gmail.com>
2024-03-18 13:43:34 +0330

    using builder callback notation

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,22 +1,34 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers } from "../redux/users/userActions";
+import { getAsyncUsers } from "../features/user/userSlice";
+// import { fetchUsers } from "../redux/users/userActions";
 
 function UsersList() {
   const dispatch = useDispatch();
-  const {loading, data, error } = useSelector((state) => state.users);
-  console.log(data);
+  const { loading, data, error } = useSelector((state) => state.user);
+  // console.log(data);
+  // this is the first method with redux
+  // useEffect(() => {
+  //   dispatch(fetchUsers());
+  // }, [dispatch]);
+  // second method with redux-toolkit
   useEffect(() => {
-    dispatch(fetchUsers());
+    dispatch(getAsyncUsers());
   }, [dispatch]);
   return (
     <div>
       <h2>Users List : </h2>
-      {loading?<p>Loading ...</p>:error?<p>{error}</p>:
-      <div>
-        {data.map(user=><li>{user.name}</li>)}
-      </div>
-      }
+      {loading ? (
+        <p>Loading ...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <div>
+          {data.map((user) => (
+            <li>{user.name}</li>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
